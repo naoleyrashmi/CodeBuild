02220508e1fb579e17a5f4ac7d0677bfcabf94c9
naoleyrashmi@gmail.com <naoley_r@India.XoriantCorp.com>
2020-01-23 17:32:52 +0530

    first commit

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 PARAMETERS_FILE="parameters.json"
 PARAMS=($(jq -r '.Parameters[] | [.ParameterKey, .ParameterValue] | "\(.[0])=\(.[1])"' ${PARAMETERS_FILE}))
-aws cloudformation deploy --template-file  /codebuild/output/packaged.yaml --region us-east-2 --capabilities CAPABILITY_IAM --stack-name InitialSetup --parameter-overrides ${PARAMS[@]}
+aws cloudformation deploy --template-file  /codebuild/output/packaged.yaml --region us-east-2 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --stack-name InitialSetup --parameter-overrides ${PARAMS[@]}
